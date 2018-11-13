46740faa1f167dd8ca910205119fd6c0f45624d1
zhuyan <yan.zhu01@bqjr.cn>
2018-11-13 14:27:28 +0800

    output

--- output.php
+++ output.php
@@ -64,14 +64,159 @@ $home = str_replace('"/public','"public',$home);
 //替换ajax请求
 $worklist = file_get_contents($serverPath . '/worklist?type=1');
 $worklist = str_replace('\/images\/','images\/',$worklist);
-echo $worklist;
-//echo unicode_decode($worklist);
-//exit;
-//echo $worklist;
 $home = str_replace('"staticHoldValue1"',$worklist,$home);
 $home = str_replace('refreshWork();',"refreshWork_static();",$home);
+
+
+/*替换模板链接*/
+$home = str_replace('href="/works"','href="works.html"',$home);
+$home = str_replace('href="/about"','href="about.html"',$home);
+$home = str_replace('href="/contact"','href="contact.html"',$home);
+$home = str_replace('href="/home"','href="home.html"',$home);
+
 file_put_contents('temp/home.html',$home);
 echo '生成首页完成<br/>';
+
+
+echo '生成works<br/>';
+$works = file_get_contents($serverPath . '/works');
+$works = str_replace('"/public','"public',$works);
+
+//替换ajax请求
+$worklist = file_get_contents($serverPath . '/worklist');
+$worklist = str_replace('\/images\/','images\/',$worklist);
+$works = str_replace('"staticHoldValue1"',$worklist,$works);
+$works = str_replace('refreshWork();',"refreshWork_static();",$works);
+
+
+/*替换模板链接*/
+$works = str_replace('href="/works"','href="works.html"',$works);
+$works = str_replace('href="/about"','href="about.html"',$works);
+$works = str_replace('href="/contact"','href="contact.html"',$works);
+$works = str_replace('href="/home"','href="home.html"',$works);
+
+$works = str_replace('href="works"','href="works.html"',$works);
+$works = str_replace('href="/works?type=2"','href="works2.html"',$works);
+$works = str_replace('href="/works?type=3"','href="works3.html"',$works);
+$works = str_replace('href="/works?type=4"','href="works4.html"',$works);
+
+
+
+file_put_contents('temp/works.html',$works);
+echo '生成works完成<br/>';
+
+$workIds = [];
+$listObj = json_decode($worklist);
+foreach ($listObj->data as $key=>$val)
+{
+    $workIds[] = $val->id;
+}
+
+
+
+echo '生成works2<br/>';
+$works = file_get_contents($serverPath . '/works');
+$works = str_replace('"/public','"public',$works);
+
+//替换ajax请求
+$worklist = file_get_contents($serverPath . '/worklist?type=2');
+$worklist = str_replace('\/images\/','images\/',$worklist);
+$works = str_replace('"staticHoldValue1"',$worklist,$works);
+$works = str_replace('refreshWork();',"refreshWork_static();",$works);
+
+
+/*替换模板链接*/
+$works = str_replace('href="/works"','href="works.html"',$works);
+$works = str_replace('href="/about"','href="about.html"',$works);
+$works = str_replace('href="/contact"','href="contact.html"',$works);
+$works = str_replace('href="/home"','href="home.html"',$works);
+
+$works = str_replace('href="works"','href="works.html"',$works);
+$works = str_replace('href="/works?type=2"','href="works2.html"',$works);
+$works = str_replace('href="/works?type=3"','href="works3.html"',$works);
+$works = str_replace('href="/works?type=4"','href="works4.html"',$works);
+
+
+
+file_put_contents('temp/works2.html',$works);
+echo '生成works2完成<br/>';
+
+
+
+
+echo '生成works3<br/>';
+$works = file_get_contents($serverPath . '/works');
+$works = str_replace('"/public','"public',$works);
+
+//替换ajax请求
+$worklist = file_get_contents($serverPath . '/worklist?type=3');
+$worklist = str_replace('\/images\/','images\/',$worklist);
+$works = str_replace('"staticHoldValue1"',$worklist,$works);
+$works = str_replace('refreshWork();',"refreshWork_static();",$works);
+
+
+/*替换模板链接*/
+$works = str_replace('href="/works"','href="works.html"',$works);
+$works = str_replace('href="/about"','href="about.html"',$works);
+$works = str_replace('href="/contact"','href="contact.html"',$works);
+$works = str_replace('href="/home"','href="home.html"',$works);
+
+$works = str_replace('href="works"','href="works.html"',$works);
+$works = str_replace('href="/works?type=2"','href="works2.html"',$works);
+$works = str_replace('href="/works?type=3"','href="works3.html"',$works);
+$works = str_replace('href="/works?type=4"','href="works4.html"',$works);
+
+
+
+file_put_contents('temp/works3.html',$works);
+echo '生成works3完成<br/>';
+
+
+
+
+echo '生成works4<br/>';
+$works = file_get_contents($serverPath . '/works');
+$works = str_replace('"/public','"public',$works);
+
+//替换ajax请求
+$worklist = file_get_contents($serverPath . '/worklist?type=4');
+$worklist = str_replace('\/images\/','images\/',$worklist);
+$works = str_replace('"staticHoldValue1"',$worklist,$works);
+$works = str_replace('refreshWork();',"refreshWork_static();",$works);
+
+
+/*替换模板链接*/
+$works = str_replace('href="/works"','href="works.html"',$works);
+$works = str_replace('href="/about"','href="about.html"',$works);
+$works = str_replace('href="/contact"','href="contact.html"',$works);
+$works = str_replace('href="/home"','href="home.html"',$works);
+
+$works = str_replace('href="works"','href="works.html"',$works);
+$works = str_replace('href="/works?type=2"','href="works2.html"',$works);
+$works = str_replace('href="/works?type=3"','href="works3.html"',$works);
+$works = str_replace('href="/works?type=4"','href="works4.html"',$works);
+
+
+
+file_put_contents('temp/works4.html',$works);
+echo '生成works4完成<br/>';
+
+
+echo '生产work详情<br/>';
+foreach ($workIds as $val)
+{
+    $work = file_get_contents($serverPath . '/work?id=' . $val);
+    $work = str_replace('"/public','"public',$work);
+    $work = str_replace('href="/works"','href="works.html"',$work);
+    $work = str_replace('href="/about"','href="about.html"',$work);
+    $work = str_replace('href="/contact"','href="contact.html"',$work);
+    $work = str_replace('href="/home"','href="home.html"',$work);
+    $work = str_replace('"/images/','"images/',$work);
+    file_put_contents('temp/work'.$val.'.html',$work);
+    echo '生产work'.$val.'<br/>';
+}
+echo '生产work详情完成<br/>';
+
 
 //拷贝资源目录
 echo '拷贝资源目录<br/>';
@@ -80,8 +225,9 @@ copyDir('public','temp/public');
 echo '拷贝资源目录结束<br/>';
 
 //生产压缩包并下载
+$zipName = time() . '' . rand(10000, 99999) . '.zip';
 $zip = new ZipArchive();
-if ($zip->open('demo.zip', ZipArchive::CREATE) === TRUE) {
+if ($zip->open($zipName, ZipArchive::CREATE) === TRUE) {
     addFileToZip('temp/', $zip); //调用方法，对要打包的根目录进行操作，并将ZipArchive的对象传递给方法
     $zip->close(); //关闭处理的zip文件
     echo 33;
@@ -91,7 +237,7 @@ if ($zip->open('demo.zip', ZipArchive::CREATE) === TRUE) {
 
 
 //header("Location:" . $serverPath . '/demo.zip');
-echo '<script>location.href="'.$serverPath . '/demo.zip'.'"</script>';
+echo '<script>location.href="'.$serverPath . '/'. $zipName .'"</script>';
 exit;
 
 //echo '生产压缩包结束<br/>';
